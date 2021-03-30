a0ecb817b7762e3b14ba76bb7101ebdab2901375
Rully Ramanda <krooco@gmail.com>
2021-03-30 12:43:16 +0700

    Adding routes

---
+++ index.ts
@@ -0,0 +1,84 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+import express, { ErrorRequestHandler } from 'express';
+import { createServer } from 'http';
+import cors from 'cors';
+import { Sequelize } from 'sequelize-typescript';
+
+const app = express();
+const server = createServer(app);
+
+app.use(express.json());
+app.use(cors());
+
+new Sequelize({
+  database: process.env.DATABASE,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  dialect: 'mysql',
+  models: [__dirname + '/models'],
+});
+
+import { User } from './models/User';
+import { Job } from './models/Job';
+import { UserJob } from './models/UserJob';
+
+app.get('/api', (req, res) => res.send('Health Check'));
+
+app.post('/api/jobs', async (req, res, next) => {
+  const { name, dayOfWeek, start, end } = req.body;
+  try {
+    await Job.create({ name, dayOfWeek, start, end });
+    return res.status(204).json();
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.post('/api/jobs/assign', async (req, res, next) => {
+  const { userId, jobId } = req.body;
+  try {
+    await UserJob.create({ userId, jobId });
+    return res.status(204).json();
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.post('/api/jobs/unassign', async (req, res, next) => {
+  const { userId, jobId } = req.body;
+  try {
+    await UserJob.destroy({ where: { userId, jobId } });
+    return res.status(204).json();
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get('/api/jobs/common', async (req, res, next) => {
+  try {
+    const jobs = await Job.findAll({
+      order: ['dayOfWeek'],
+      include: [
+        {
+          model: User,
+          attributes: { exclude: ['password'] }
+        }
+      ],
+    });
+    return res.status(200).json(jobs);
+  } catch (error) {
+    next(error);
+  }
+});
+
+const errorHandler: ErrorRequestHandler = (error, _req, res, next) => {
+  return res.status(500).json({ error: error.message });
+};
+
+app.use(errorHandler);
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at: https://localhost:${PORT}`);
+});
